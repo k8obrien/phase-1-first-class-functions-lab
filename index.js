c7bfefb28437bcef8f77904f0fc460feae974047
kateobrien <k8obrien2811@gmail.com>
2021-09-19 21:00:50 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const driversArray = new Array ("Max", "Jasper", "Brody", "Riley");
+const returnFirstTwoDrivers = function(driversArray){
+    return driversArray.slice(0,2)
+}
+const returnLastTwoDrivers = function(driversArray){
+    return driversArray.slice(2,4)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(interger){
+    return function price(){
+        return (interger * interger)
+    }
+}
+const fareDoubler = interger => interger * 2
+const fareTripler = interger => interger * 3
+
+function selectDifferentDrivers(driversArray, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(driversArray)
+}
